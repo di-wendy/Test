881ef15ca289ebec039284ccefdeafba8b557472
dwang3 <di_wang@intuit.com>
2018-04-03 14:06:08 -0700

    init

---
+++ src/components/ColorTool.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import { ToolHeader } from './ToolHeader';
+import { ColorForm } from './ColorForm';
+
+export class ColorTool extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: this.props.colors.concat(),
+    };
+  }
+
+  addColor = (newColor) => {
+    this.setState({
+      colors: this.state.colors.concat(newColor),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ToolHeader headerText="Color Tool" />
+        <ul>{this.state.colors.map(color => <li key={color}>{color}</li>)}</ul>
+        <ColorForm onSubmitColor={this.addColor} />
+      </div>
+    );
+  }
+}
