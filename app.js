991fbc8be8cdfe8cb2741eeee9e51125efe04630
dwang3 <di_wang@intuit.com>
2018-04-17 11:08:44 -0700

    Basic databse set up and routing

--- app.js
+++ app.js
@@ -1,36 +1,74 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set('view engine','ejs');
 
-var grounds = [
-    {name:'wendy', image:'https://www.gstatic.com/webp/gallery/4.sm.webp', },
-    {name:'Hao', image:'https://www.gstatic.com/webp/gallery/4.sm.webp'}
-]
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+})
 
-//GET
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//INDEX ROUTE
 app.get('/',function(req, res){
     res.render('landing');
 })
 
+//NEW
 app.get('/campgrounds/new', function(req, res){
     res.render('new')
 })
 
+//SHOW - show more info
+app.get('/campgrounds/:id', function(req, res){
+   Campground.findById(req.param.id, function(err, foundCampground){
+        if(err){
+            console.log(err)
+        }
+        else{
+            req.params.id
+            res.render('show', {campground:foundCampground});
+        }
+   });
+})
+
 app.get('/campgrounds', function(req, res){
-    res.render('campgrounds',{campgrounds:grounds});
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, Allcampgrounds){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render('campgrounds',{campgrounds:Allcampgrounds});
+        }
+    }) 
 });
 
 //POST
 app.post('/campgrounds',function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCamp = {name: name, image: image}
-    grounds.push(newCamp);
-    res.redirect('/campgrounds');
+    var desc = req.body.description;
+    var newCamp = {name: name, image: image, description:desc};
+    //Create a new  campground and save to DB
+    Campground.create(
+        newCamp,
+        function(err, campground){
+            if(err){
+                console.log(err)
+            } else{
+                res.redirect('/campgrounds');
+            }
+        }
+    )
 })
 
 app.listen(3000,function(){
